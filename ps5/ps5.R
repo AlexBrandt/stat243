1c932a977d358bec5329b62d39ddd0967b50ca8a
Alexander Brandt <Alex@Alexanders-MacBook-Pro.local>
2015-10-19 13:08:11 -0700

    ps5 commit

--- ps5/ps5.R
+++ ps5/ps5.R
@@ -1,32 +1,57 @@
 # 1 a)
 
-# The number of precision is 1.000000000001 is 12 (the number of digits after the decimal).
+# The number of precision is 1.000000000001 is 13from decimal import *
+# NOTE: all python code is just embedded in the .Rnw doc
 
-# 1 b) 
-
+options(digits=22)
+# Create the list, init an empty sum variable, then sum
 numbers <- c(1, rep(1 * 10^(-16), 10000))
 sum(numbers)
 
-# Creates just one
-
-# 1 d)
-
-my_sum <- numbers[1] + numbers[2]
-for (i in 3:length(numbers))
+options(digits=22)
+# Create the list, init an empty sum variable, then loop
+# using the previous numbers list/array
+my_sum <- 0
+for (i in 1:length(numbers))
 {
-    my_sum <- my_sum + numbers[i]
+  my_sum <- my_sum + numbers[i]
 }
 my_sum
 
-# Doesn't work...
-
-# 1 e) works!
-
+options(digits=22)
+# Create the list, init an empty sum variable, then loop
 revised_numbers <- c(rep(1 * 10^(-16), 10000), 1)
-my_revised_sum <- revised_numbers[1] + revised_numbers[2]
-for (i in 3:length(revised_numbers))
+my_revised_sum <- 0.0
+for (i in 1:length(revised_numbers))
 {
-  my_revised_sum <- my_revised_sum + numbers[i]
+  my_revised_sum <- my_revised_sum + revised_numbers[i]
 }
 my_revised_sum
+
+# Find out where 'sum' goes...
+sum
+
+# 2) testing speeds
+
+options(digits=6)
+v <- rep(as.integer(1), 200000000)
+system.time(sum(v))
+
+v <- rep(as.double(1), 200000000)
+system.time(sum(v))
+
+v <- rep(as.integer(1:5), 20000000)
+system.time(v[(v %% 5) == 0])
+
+v <- rep(as.double(1:5),  20000000)
+system.time(v[(v %% 5) == 0 ])
+
+options(digits=6)
+v <- rep(as.integer(1), 2000000)
+system.time(sapply(v, is.integer))
+
+v <- rep(as.double(1), 2000000)
+system.time(sapply(v, is.integer))
+
+
 
